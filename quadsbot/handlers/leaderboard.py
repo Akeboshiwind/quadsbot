b5915b2698bbf0559f764f2c4e7ae120a4d46295
Oliver Marshall <olivershawmarshall@gmail.com>
2022-04-20 09:35:20 +0100

    Separate out message building logic for the leaderboard

--- quadsbot/handlers/leaderboard.py
+++ quadsbot/handlers/leaderboard.py
@@ -6,6 +6,36 @@ from telegram.ext import CallbackContext
 from quadsbot.handlers.message import message_handler, State
 
 
+def leaderboard(bot_data) -> str:
+    # Get user objects, sorted by score
+    score_field = "checked_unique"
+    scores = [score for _, score in bot_data.items()]
+    scores = sorted(scores, key=lambda s: s[score_field], reverse=True)
+
+    # Create message w/ Header
+    message = "<b>Leaderboard</b>\n"
+
+    if len(scores) >= 1:
+        # Format top scorer specially
+        top = scores[0]
+        top_score = top[score_field]
+        top_user = top["username"]
+
+        message += f"1. {top_score} - 👑 <b>{top_user}</b> 👑"
+
+        # Add the rest of the scorers
+        for idx, entry in enumerate(scores[1:]):
+            # Convert to correct numbering
+            place = idx + 2
+            score = entry[score_field]
+            username = entry["username"]
+            message += f"\n{place}. {score} - {username}"
+    else:
+        message += "<i>Empty...</i>"
+
+    return message
+
+
 def leaderboard_handler(update: Update, context: CallbackContext) -> None:
     """
     Display a leaderboard of scores
@@ -20,26 +50,6 @@ def leaderboard_handler(update: Update, context: CallbackContext) -> None:
         if state == State.DELETE:
             return
 
-    score_field = "checked_unique"
-    scores = [score for _, score in context.bot_data.items()]
-    scores = sorted(scores, key=lambda s: s[score_field], reverse=True)
-
-    message = "<b>Leaderboard</b>\n"
-
-    if len(scores) >= 1:
-        top = scores[0]
-        top_score = top[score_field]
-        top_user = top["username"]
-
-        message += f"1. {top_score} - 👑 <b>{top_user}</b> 👑"
-
-        for idx, entry in enumerate(scores[1:]):
-            # Convert to correct numbering
-            idx += 2
-            score = entry[score_field]
-            user = entry["username"]
-            message += f"\n{idx}. {score} - {user}"
-    else:
-        message += "<i>Empty...</i>"
-
+    # Reply with the leaderboard
+    message = leaderboard(context.bot_data)
     update.message.reply_html(message)
